58ec055165addb7ee4eeb0df8ba08727d4f61c0d
Andrew Stein <ajsforlife@icloud.com>
2022-06-14 15:21:56 -0600

    adding pypoll.py file

---
+++ module 3/python_practice(loops)10.py
@@ -0,0 +1,7 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+#for num in numbers:
+#    print(num)
+#for num in range(5):
+#    print(num)
+for i in range(len(counties)):
+    print(counties[i])
---
+++ module 3/python_practice8.py
@@ -0,0 +1,5 @@
+#nothing will print condition is false
+count = 7
+while count < 1:
+    print("Hello world")
+    
